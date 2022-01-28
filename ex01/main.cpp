d9b444eee80f6904db7b661eca47b77179cf09d5
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-01-28 16:51:26 +0900

    Chaptur

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,6 +3,12 @@
 
 int main(void)
 {
-	printf_s("This is a test");
+	int a = 15, b = 20;
+
+	printf_s("a=15\nb=20\n");
+	printf_s("a+b=%d\n", a + b);
+	printf_s("‰½‚©ƒL[‚ğ‰Ÿ‚µ‚Ä‚­‚¾‚³‚¢\n");
+	_getch();
+	printf_s("End");
 	return 0;
 }
