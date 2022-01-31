3d7a45b63cef94bfeba36cb4fde6730adb05e64d
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-01-31 17:35:57 +0900

    Chapter 8 bonus

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,17 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
 
-int input_isbn(void);
-
-int main(void)
-{
-	input_isbn();
-
-	printf_s("何かキーを押してください\n");
-	_getch();
-	printf_s("End");
-	return 0;
-}
 
 int input_isbn(void)
 {
@@ -20,5 +9,15 @@ int input_isbn(void)
 	printf_s("ISBNを入力してください。\n");
 	scanf_s("%s", isbn, 64);
 	printf_s("入力された番号は、%sです。\n", isbn);
+	return 0;
+}
+
+int main(void)
+{
+	input_isbn();
+
+	printf_s("何かキーを押してください\n");
+	_getch();
+	printf_s("End");
 	return 0;
 }
