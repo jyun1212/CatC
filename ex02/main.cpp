37fd0d34638f350d1b0b1e25cd9ff2dfc56cb09d
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-02 22:09:43 +0900

    Chapter 11-4

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-	int a = 2, b, c;
+	int i = 6, j = 6;
 
-	b = (a == 3);
-	c = (a == 2);
-	
-	printf_s("b=%d,c=%d\n", b, c);
+	printf_s("i=%d\n", i++);	//‚±‚Ì“_‚Åi‚Í6
+	printf_s("i=%d\n", i);		//i‚Í7‚É‚È‚Á‚Ä‚¢‚é
+	printf_s("j=%d\n", ++j);	//j‚Í‚±‚Ì“_‚Å7
+	printf_s("j=%d\n", j);		//7‚©‚ç•ÏX‚È‚µ
 	return 0;
 }
