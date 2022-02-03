77886f5974e13e81f4b5f1dee41ca88667ee142d
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-03 22:21:25 +0900

    Chapter 12

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,12 +1,56 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<process.h>
 
-int main()
+void question_create(void);			//問題作成
+void input_answer(void);			//答えを入力
+void jadge(void);					//答えの入力の正誤判定
+
+int a, b, c;	//グローバル変数
+
+void main()
 {
-	int i = 6, j = 6;
+	printf_s("やめるときは、99と入力してください\n");
+	while (1)
+	{//無限ループ
+		question_create();
+		input_answer();
+		jadge();
+	}
+	return;
 
-	printf_s("i=%d\n", i++);	//この時点でiは6
-	printf_s("i=%d\n", i);		//iは7になっている
-	printf_s("j=%d\n", ++j);	//jはこの時点で7
-	printf_s("j=%d\n", j);		//7から変更なし
-	return 0;
+}
+
+
+void question_create(void)
+{
+	a = rand() % 10;
+	b = rand() % 10;
+	printf_s("%d かける %d は ", a, b);
+	return;
+}
+
+void input_answer(void)
+{
+	scanf_s("%d", &c);
+	if (c == 99)
+	{
+		exit(0);
+	}
+
+	return;
+}
+
+void jadge(void)
+{
+	if (c == a * b)
+	{
+		printf_s("正解です。\n");
+	}
+	else
+	{
+		printf_s("不正解です。\n");
+	}
+	printf_s("\n");
+	return;
 }
