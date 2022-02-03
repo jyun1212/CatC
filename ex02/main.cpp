9a5d1341036c620555047cd1638adc95480c7f82
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-04 01:11:42 +0900

    Chapter 12 bonus

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 #include<process.h>
 
 void question_create(void);			//問題作成
@@ -7,15 +8,48 @@ void input_answer(void);			//答えを入力
 void jadge(void);					//答えの入力の正誤判定
 
 int a, b, c;	//グローバル変数
+int i;			//イテレータ
+int clear_num;	//正答数
 
 void main()
 {
-	printf_s("やめるときは、99と入力してください\n");
+	
 	while (1)
 	{//無限ループ
-		question_create();
-		input_answer();
-		jadge();
+		LOOPy:
+		printf_s("\n九九5問\n\n");
+		clear_num = 0;
+
+		for (i = 0; i < 5; i++)
+		{//5問で一区切り
+			question_create();
+			input_answer();
+			jadge();
+			if (i >= 4)
+			{
+				printf_s("正答は%d問です。\n\n", clear_num);
+				//継続確認
+				printf_s("もう一度？\nやる(y) やめる(n)");
+
+			LOOPdef:
+				switch (_getch())
+				{
+				case 'y':
+					puts("");
+					goto LOOPy;
+					break;
+
+				case 'n':
+					exit(0);
+					break;
+
+				default:
+					goto LOOPdef;
+					break;
+				}
+			}
+		}
+
 	}
 	return;
 
@@ -33,11 +67,6 @@ void question_create(void)
 void input_answer(void)
 {
 	scanf_s("%d", &c);
-	if (c == 99)
-	{
-		exit(0);
-	}
-
 	return;
 }
 
@@ -46,6 +75,7 @@ void jadge(void)
 	if (c == a * b)
 	{
 		printf_s("正解です。\n");
+		clear_num++;
 	}
 	else
 	{
