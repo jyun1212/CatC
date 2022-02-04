51ed99048cf10e5819106d6f55b7bc03f7e5cc24
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-04 20:55:25 +0900

    Chapter 13

--- Time01/time01.cpp
+++ Time01/time01.cpp
@@ -5,12 +5,15 @@ void main()
 {
 	int i = 1;		//iterator
 	time_t t1, t2;
-	time(&t1);		//t1‚É•\¦‘O‚Ì•b”‚ğŠi”[
+	time(&t1);		//t1‚É•\¦‘O‚Ì•b”(1970/1/1~Œ»İ)‚ğŠi”[
 	
 	while (i <= 5000)
-	{
+	{//5000‚Ü‚Å
 		printf_s("%d,", i++);
 	}
 	time(&t2);		//t2‚É•\¦Œã‚Ì•b”‚ğŠi”[
-	printf("\ntime1=%ld time2=%ld\n", t1, t2);
+	printf_s("\ntime1=%ld time2=%ld\n", t1, t2);
+	printf_s("‚©‚©‚Á‚½ŠÔ‚ÍA%d•b‚Å‚·B\n", t2 - t1);
+	return;
+
 }
