ba3df0bc86f870c8cb9f7026775eb8b5c2a91c24
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-05 17:00:59 +0900

    Chapter 16

--- Time01/time01.cpp
+++ Time01/time01.cpp
@@ -1,50 +1,13 @@
 #include<stdio.h>
-#include<string.h>
 
-void nextStruct(void);
-
-struct employee
+void main()
 {
-	int age;
-	char name[32];
-	char sex;
-};
+	int a = 12;
+	int* ptr;
 
-int main()
-{
-	struct employee a, b;
-	
-	a.age = 32;
-	strcpy_s(a.name, "粂井康孝");
-	a.sex = 'M';
-
-	b.age = 35;
-	strcpy_s(b.name, "服屁津田");
-	b.sex = 'F';
-
-	printf_s("社員aの 名前:%s 年齢%d 性別%c\n", a.name, a.age, a.sex);
-	printf_s("社員bの 名前:%s 年齢%d 性別%c\n", b.name, b.age, b.sex);
-	puts("");
-
-	printf_s("別な関数の社員aの名前は・・・\n");
-	nextStruct();
-	puts("");
-	
-	printf_s("main関数の社員aの名前は・・・\n");
-	printf_s("社員aの 名前:%s\n", a.name);
-
-	return 0;
-}
-
-void nextStruct()
-{
-	struct employee a;		//main関数のaとは異なる
-
-	a.age = 55;
-	strcpy_s(a.name, "佐藤清澄");
-	a.sex = 'M';
-
-	printf("社員aの 名前:%s\n", a.name);
-	
+	ptr = &a;		//変数aのアドレスをptrに代入
+	printf_s("a=%d\n", *ptr);
+	a = *ptr * 3;	//ptrのさす内容を3倍してaに代入
+	printf_s("a=%d\n", a);
 	return;
 }
