c65828ff5d85591ac3e282f0325c97fb16955362
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-05 16:32:44 +0900

    Chapter 15

--- Time01/time01.cpp
+++ Time01/time01.cpp
@@ -1,33 +1,50 @@
 #include<stdio.h>
+#include<string.h>
+
+void nextStruct(void);
+
+struct employee
+{
+	int age;
+	char name[32];
+	char sex;
+};
 
 int main()
 {
-	int i,j;	//iterator
-	int count_border = 0;
+	struct employee a, b;
+	
+	a.age = 32;
+	strcpy_s(a.name, "粂井康孝");
+	a.sex = 'M';
 
-	for (i = 1; i <= 9; i++)
-	{
-		printf_s("%2d ", i);	//%2dでiが2桁表示
-		count_border += 3;
-	}
-	printf_s("\n");
+	b.age = 35;
+	strcpy_s(b.name, "服屁津田");
+	b.sex = 'F';
 
-	for (i = 1; i <= count_border; i++)
-	{
-		printf_s("-");
-	}
-	printf_s("\n");
+	printf_s("社員aの 名前:%s 年齢%d 性別%c\n", a.name, a.age, a.sex);
+	printf_s("社員bの 名前:%s 年齢%d 性別%c\n", b.name, b.age, b.sex);
+	puts("");
 
-	for (i = 1; i <= 9; i++)
-	{
-		for (j = 1; j <= 9; j++)
-		{
-			printf_s("%2d ", i * j);
-			if (j == 9)
-			{//改行
-				printf_s("\n");
-			}
-		}
-	}
+	printf_s("別な関数の社員aの名前は・・・\n");
+	nextStruct();
+	puts("");
+	
+	printf_s("main関数の社員aの名前は・・・\n");
+	printf_s("社員aの 名前:%s\n", a.name);
+
 	return 0;
+}
+
+void nextStruct()
+{
+	struct employee a;		//main関数のaとは異なる
+
+	a.age = 55;
+	strcpy_s(a.name, "佐藤清澄");
+	a.sex = 'M';
+
+	printf("社員aの 名前:%s\n", a.name);
+	
+	return;
 }
