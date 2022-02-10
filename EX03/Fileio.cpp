cf9105057a0f14faa030120f03ef32c0eb511be1
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-11 00:30:24 +0900

    Chapter 19

--- EX03/Fileio.cpp
+++ EX03/Fileio.cpp
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include<process.h>
+#include<stdlib.h>
 #include<locale.h>
 
 int main()
 {
 	FILE* fp;
 	errno_t error;
-	char str[256];
+	char str[1024];
 	str[sizeof(str) - 1] = '\0';
 
 	//ファイルを開く
-	error = fopen_s(&fp,"test.txt", "r");
+	error = fopen_s(&fp,"myfile.txt", "a");
 
 	if (error != 0)
 	{//失敗したら中止
@@ -18,16 +19,31 @@ int main()
 		exit(-1);
 	}
 
+	printf_s("ファイルに追加するテキストを入力してください。\n");
+	printf_s("Eと入力すると終了です。\n");
+
 	//UTF-8文字化け対策
 	setlocale(LC_CTYPE, ".65001");
-	while (fscanf_s(fp, "%s", str,256) != EOF)
-	{//ファイルの最後まで到達しない限り繰り返す
-		printf_s(str);
-		puts("");	//改行
+
+	while (1)
+	{
+		
+		scanf_s("%s", str, 1024);		//ファイルに追加する文章の入力
+		if (str[0] == 'E')
+		{//1文字目がEならループ脱出
+			break;
+		}
+
+		fprintf_s(fp, "%s", str);		//ファイルに書き込み
+		fprintf_s(fp, "%c", '\n');		//見やすくするために改行を書き込む
+		printf_s("ファイルにテキストを追加しました。\n");
+
 	}
 
 	//ファイルを閉じる
 	fclose(fp);
+
+	system("type myfile.txt");
 
 	return 0;
 }
