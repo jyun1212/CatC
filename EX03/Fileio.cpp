8f30b22d30e8f44172adf75fd2883195a99aa67c
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-19 20:50:06 +0900

    Chapter 21 avan

--- EX03/Fileio.cpp
+++ EX03/Fileio.cpp
@@ -1,62 +1,29 @@
+//間違ったプログラム
+
 #include<stdio.h>
-#include<string.h>
-#include<locale.h>
+int sumPrice(int, int, int);
 
-int file_read(FILE*);
-
-int main(int argc, char* argv[])
+int main()
 {
-	FILE* fp;
-	errno_t error;
+	int price;		//金額(kingaku)
+	int tax;		//消費税(zeiritsu)
+	int sumprice;	//定価(nedam)
 
-	char fname[64];
-	fname[sizeof(fname) - 1] = '\0';
+	printf_s("金額を入力してください = ");
+	scanf_s("%d", &price);
+
+	printf_s("\n消費税率を入力してください(%%) = ");
+	scanf_s("%d", &tax);
 	
-
-	if (argc == 1)
-	{
-		printf_s("コマンドラインからファイル名を入力できます！\n");
-		printf_s("ファイル名=");
-		scanf_s("%s", fname,64);
-	}
-	else
-	{
-		strcpy_s(fname, argv[1]);
-	}
-
-	while (1)
-	{
-		error = fopen_s(&fp,fname,"r");
-
-		if (error != NULL)
-		{
-			printf_s("ファイル名が不当です\n");
-			printf_s("ファイル名=");
-			scanf_s("%s", fname, 64);
-		}
-		else
-		{
-			break;
-		}
-	}
-
-	file_read(fp);
-
-	if (fp != 0) {
-	fclose(fp);
-	}
+	sumPrice(price, tax, sumprice);
+	printf_s("%dに%d%の消費税を加えると%d円になる\n", price, tax, sumprice);
 
 	return 0;
 }
 
-int file_read(FILE* f)
-{
-	int ch;
 
-	setlocale(LC_CTYPE, ".65001");
-	while ((ch = getc(f)) != EOF)
-	{
-		putc(ch, stdout);
-	}
+int sumPrice(int price, int tax, int sumprice)
+{
+	sumprice = price * (1 + tax / 100);
 	return 0;
 }
