47b6aa489c07a6eefa85d08b669fb01cfb1614cb
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-21 22:49:48 +0900

    Chapter 22 いろいろな配列

--- EX03/Fileio.cpp
+++ EX03/Fileio.cpp
@@ -1,30 +1,42 @@
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
-	int a[3];
+	int aryNum;		//イテレータ
+	int aryNumMax;	//配列の要素数
 
-	a[0] = 10;
-	a[1] = 15;
-	a[2] = 20;
-	a[3] = 30;	//現代だとエラーが出て実行不可
 
-	for (int i = 0; i <= 3; i++)
+	printf_s("例題1\n");
+	static int a[4][2] = { 8, 7, 6, 5, 4, 3, 2, 1 };
+
+	for (int ary_line = 0; ary_line < 4; ary_line++)
 	{
-		printf_s("a[%d]= %d\n", i, a[i]);
+		for (int ary_ele = 0; ary_ele < 2; ary_ele++)
+		{
+			printf_s("%d", a[ary_line][ary_ele]);
+		}
+		puts("");
 	}
 
+	printf_s("\n例題2\n");
+	static char b[] = { 'A','B','C','D' };
+	aryNumMax = sizeof b / sizeof b[0];
 
+	for (aryNum = 0; aryNum < aryNumMax; aryNum++)
+	{
+		printf_s("%c", b[aryNum]);
+	}
 
+	printf_s("\n例題3\n");
+	const char* str = "ABCD";
 
+	aryNumMax = strlen(str);
 
-
-
-
-
-
-
-
+	for (aryNum = 0; aryNum < aryNumMax; aryNum++)
+	{
+		printf_s("str[%d]='%c'\n", aryNum, str[aryNum]);
+	}
 
 
 	return 0;
