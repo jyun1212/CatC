176a2d60c90aefd736a4d353b5e56b1d18c13700
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-02-25 00:36:43 +0900

    Chapter 23 配列とポインタ

--- EX03/Fileio.cpp
+++ EX03/Fileio.cpp
@@ -1,43 +1,27 @@
 #include<stdio.h>
-#include<string.h>
 
 int main()
 {
-	int aryNum;		//イテレータ
-	int aryNumMax;	//配列の要素数
+	int i, a[4], * ptr;
 
+	a[0] = 15;
+	a[1] = 20;
+	a[2] = 25;
+	a[3] = 10;
 
-	printf_s("例題1\n");
-	static int a[4][2] = { 8, 7, 6, 5, 4, 3, 2, 1 };
-
-	for (int ary_line = 0; ary_line < 4; ary_line++)
+	ptr = &a[0];
+	for (i = 0; i <= 3; i++)
 	{
-		for (int ary_ele = 0; ary_ele < 2; ary_ele++)
-		{
-			printf_s("%d", a[ary_line][ary_ele]);
-		}
-		puts("");
+		printf_s("a[%d]=%d\n", i, a[i]);
+
+		printf_s("*(&a[%d])=%d\n", i, *(&a[i]));
+		
+		printf_s("*(&a[0]+%d)=%d\n", i, *(&a[0] + i));
+		
+		printf_s("*(ptr+%d)=%d\n", i, *(ptr + i));
+		
+		printf_s("*(a+%d)=%d\n", i, *(a + i));
 	}
-
-	printf_s("\n例題2\n");
-	static char b[] = { 'A','B','C','D' };
-	aryNumMax = sizeof b / sizeof b[0];
-
-	for (aryNum = 0; aryNum < aryNumMax; aryNum++)
-	{
-		printf_s("%c", b[aryNum]);
-	}
-
-	printf_s("\n例題3\n");
-	const char* str = "ABCD";
-
-	aryNumMax = strlen(str);
-
-	for (aryNum = 0; aryNum < aryNumMax; aryNum++)
-	{
-		printf_s("str[%d]='%c'\n", aryNum, str[aryNum]);
-	}
-
 
 	return 0;
 }
