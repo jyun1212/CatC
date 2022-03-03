c02946736ca7a693f672c5010c8888a720a5bc7c
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-03-03 20:48:16 +0900

    Chapter 26-1

--- EX03/Fileio.cpp
+++ EX03/Fileio.cpp
@@ -1,25 +1,18 @@
 #include<stdio.h>
 #include<time.h>
-#include<Time.h>
 
 int main()
 {
-	time_t present_tm;	//longと考えて良い
-	struct tm *my_time;	//tm構造体へのポインタ
-	char* now_time;
+	time_t present_tm;	//time_t == long
+	struct tm my_time;	//tm構造体へのポインタ
 
-	errno_t error;
-	error = localtime_s(&now_time, &present_tm);
+	time(&present_tm);	//経過時間取得
 
-	if (error != 0)
-	{
-		time(&present_tm);	//経過時間取得
-		my_time = localtime_s(&now_time, &present_tm);
+	localtime_s(&my_time, &present_tm);
 
-		printf_s("%d %d %d %d %d %d %d",
-			now_time.tm_year, now_time.tm_mon, now_time.tm_mday,
-			now_time.tm_hour, now_time.tm_min, now_time.tm_sec);
-	}
+	printf_s("%ld/%ld/%ld %ld:%ld:%ld\n",
+		my_time.tm_year + 1900, my_time.tm_mon + 1, my_time.tm_mday,
+		my_time.tm_hour, my_time.tm_min, my_time.tm_sec);
 
 	return 0;
 }
