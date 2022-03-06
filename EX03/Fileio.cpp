21b401ef887d81b9419dffd05a7c578d54423356
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-03-06 23:34:38 +0900

    Chaputer 26 asctime使用ver. ほぼ公式ドキュメント

--- EX03/Fileio.cpp
+++ EX03/Fileio.cpp
@@ -1,18 +1,22 @@
-#include<stdio.h>
-#include<time.h>
+#include <time.h>
+#include <stdio.h>
 
-int main()
+struct tm newtime;
+time_t aclock;
+
+int main(void)
 {
-	time_t present_tm;	//time_t == long
-	struct tm my_time;	//tm構造体へのポインタ
+    char buffer[32];
+    errno_t errNum;
+    time(&aclock);   // Get time in seconds.
+    localtime_s(&newtime, &aclock);   // Convert time to struct tm form.
 
-	time(&present_tm);	//経過時間取得
+    // Print local time as a string.
 
-	localtime_s(&my_time, &present_tm);
-
-	printf_s("%ld/%ld/%ld %ld:%ld:%ld\n",
-		my_time.tm_year + 1900, my_time.tm_mon + 1, my_time.tm_mday,
-		my_time.tm_hour, my_time.tm_min, my_time.tm_sec);
-
-	return 0;
+    errNum = asctime_s(buffer, 32, &newtime);
+    if (errNum != 1)
+    {
+        printf("Current date and time: %s", buffer);  
+    }
+    return 0;
 }
