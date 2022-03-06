8d959a1233289312ef873746888ba61780d7729c
jyun1212 <jyunya@khh.biglobe.ne.jp>
2022-03-07 00:29:48 +0900

    Chapter 26-3

--- EX03/Fileio.cpp
+++ EX03/Fileio.cpp
@@ -1,23 +1,27 @@
-#include<stdio.h>
+#include <time.h>
+#include<conio.h>
+#include <stdio.h>
 
-int main()
+struct tm newtime;
+time_t aclock;
+
+int main(void)
 {
-	printf_s("\x1b[4;31m");
-	printf_s("どうです色が変わってアンダーラインもついたでしょう！\n");
-	
-	printf_s("\x1b[7;32m");
-	printf_s("今度は反転です。\n");
-	
-	putchar(0x1b);
-	
-	printf_s("[0;36m");
-	printf_s("今度は水色です\n");
+	char now_time[32];
+	errno_t errNum;
 
-	printf_s("%c%s今度は緑\n", 0x1b, "[32m");
-	printf_s("%c%s%s\n", 0x1b, "[1;35m", "こんな書き方もあります\n");
-	
-	printf_s("\x1b[0m");
+	while (!_kbhit())
+	{
+		time(&aclock);
+		localtime_s(&newtime, &aclock);
+
+		errNum = asctime_s(now_time, 32, &newtime);
+		if (errNum != 1)
+		{
+			printf_s("\x1b[4;1H");
+			printf_s("now_time=%s\n", now_time);
+		}
+	}
 
 	return 0;
-
 }
